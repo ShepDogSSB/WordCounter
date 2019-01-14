758ddda9b35b9a3620d9dcf42973fa89b9871118
ShepDogSSB <nickshepard288150@gmail.com>
2019-01-14 13:50:08 -0500

    Splits the file given into words, counts those words. puts each unique word and how many occurances of that word are in the given file.

--- WordCounter/src/WordCounter.java
+++ WordCounter/src/WordCounter.java
@@ -1,5 +1,7 @@
 import components.map.Map;
 import components.map.Map1L;
+import components.sequence.Sequence;
+import components.sequence.Sequence1L;
 import components.simplereader.SimpleReader;
 import components.simplereader.SimpleReader1L;
 import components.simplewriter.SimpleWriter;
@@ -46,20 +48,70 @@ public final class WordCounter {
                 + inputName + "</h2>" + "<hr>");
     }
 
-    public static void getWords(Map<String, Integer> data, SimpleReader in) {
+    /**
+     *
+     */
+    public static void removePuncuation(Sequence<String> listOfWords) {
+        for (int i = 0; i < listOfWords.length(); i++) {
+            String testVal = listOfWords.entry(i);
+            if (testVal.equals(",")) {
+                listOfWords.remove(i);
+            }
 
-        /*
-         * While we can still parse through the input
-         */
+            if (testVal.contains(".")) {
+                listOfWords.remove(i);
+                listOfWords.add(i, testVal.replace(".", ""));
+            } else if (testVal.contains(",")) {
+                listOfWords.remove(i);
+                listOfWords.add(i, testVal.replace(",", ""));
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public static void createListOfWords(Sequence<String> listOfWords,
+            String lineOfTxt) {
+        int x = 0;
+        for (int i = lineOfTxt.indexOf(' '), j = 0; i != -1;) {
+            listOfWords.add(x, lineOfTxt.substring(j, i));
+            j = i + 1;
+            i = lineOfTxt.indexOf(' ', j);
+            x++;
+        }
+        String test = lineOfTxt.substring(lineOfTxt.lastIndexOf(' ') + 1,
+                lineOfTxt.length());
+        listOfWords.add(x, test);
+        x = 0;
+    }
+
+    /**
+     *
+     *
+     */
+    public static void parseFile(Map<String, Integer> data, SimpleReader in) {
         while (!in.atEOS()) {
-            /*
-             * We test to see if the next line has a space in it and if the line
-             * is an empty string, if it doesn't have either, then we enqueue it
-             * to the words queue.
-             */
-            String testVal = in.nextLine();
-            if (!(testVal.contains(" ") && testVal.isEmpty())) {
+            Sequence<String> listOfWords = new Sequence1L<>();
+            String lineOfTxt = in.nextLine();
+            lineOfTxt = lineOfTxt.replace("   ", "");
+            lineOfTxt = lineOfTxt.replace("--", " ");
+            lineOfTxt = lineOfTxt.replace("-", " ");
 
+            createListOfWords(listOfWords, lineOfTxt);
+            removePuncuation(listOfWords);
+
+            for (int i = 0; i < listOfWords.length(); i++) {
+                if (!data.hasKey(listOfWords.entry(i))) {
+                    data.add(listOfWords.entry(i), 0);
+                }
+            }
+
+            for (int i = 0; i < listOfWords.length(); i++) {
+                int count = data.value(listOfWords.entry(i));
+                if (data.hasKey(listOfWords.entry(i))) {
+                    data.replaceValue(listOfWords.entry(i), count + 1);
+                }
             }
         }
     }
@@ -86,15 +138,15 @@ public final class WordCounter {
          */
 
         out.println("Enter The Location & Name of Input File: ");
-        String inputName = in.nextLine();
+        String inputName = "data/gettysburg.txt";//in.nextLine();
         SimpleReader fileIn = new SimpleReader1L(inputName);
 
         /*
          * Reads in the name of the output folder
          */
 
-        out.println("Enter The Name of the Output Folder: ");
-        String folder = in.nextLine();
+        out.println("Enter The Location & Name of Output File: ");
+        String folder = "data/gettysburg.html";//in.nextLine();
 
         /*
          * Makes a copy of the input file string, removes the ".txt" file
@@ -110,15 +162,18 @@ public final class WordCounter {
          * itself.
          */
 
-        SimpleWriter index = new SimpleWriter1L(
-                folder + "/" + fileName + ".html");
+        SimpleWriter index = new SimpleWriter1L(folder);
         createHeader(index, inputName);
 
         /*
          * Gets the words & puts them into the map
          */
 
-        getWords(data, fileIn);
+        parseFile(data, fileIn);
+        data.remove("");
+        for (Map.Pair<String, Integer> x : data) {
+            out.println(x);
+        }
 
         in.close();
         out.close();
